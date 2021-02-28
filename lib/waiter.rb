14f9704a12417e9d9e7176ef2fc9e3d331736ab8
Adriana <adriana.amaral@msn.com>
2021-02-28 17:21:38 -0600

    commit tree 77022a8a79822d6c7d0861cdb2a4b614c1ec35a3

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
 class Waiter
 
+    attr_reader :name, :years_of_experience
+    
+    @@all = []
+  
+def initialize (name, years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience 
+    @@all << self 
+end 
+
+def self.all
+    @@all
+end 
+
+def new_meal (customer, total, tip)
+    Meal.new(self, customer, total, tip)
+end 
+
+def meals 
+    Meal.all.select do |meal|
+    meal.waiter == self 
+end 
+end 
+
+def best_tipper
+    best_tip = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip 
+      #TO REMEMBER <=>
+      #It's a general comparison operator. 
+      #It returns either a -1, 0, or +1 depending on whether its receiver is less than, 
+      #equal to, or greater than its argument.
+    end
+    best_tip.customer
+  end
+  
+
 end
